8b292eb45338d981a5d81901de1a0f264327ebfb
xinxiang <xiang.wang@bozone.mobi>
2012-03-18 22:13:18 +0800

    Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
+#coding: utf-8
 class Product < ActiveRecord::Base
+	validates :title, :description, :image_url, :presence=>true
+	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+	validates :title, :uniqueness => true #唯一
+	validates :image_url, :format =>{
+		:with => %r{\.(gif|jpg|png)$}i,
+		:message => '图片必需是GIF, JPG 或者 PNG 格式的URL!'
+	}
 end
+
+  
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,5 +15,5 @@ Ruby is the fastest growing and most exciting dynamic language
 out there. If you need to get working programs delivered fast,
 you should add Ruby to your toolbox.
 </p>},
-:image_url => '/images/ruby.jpg',
+:image_url => '/assets/ruby.jpg',
 :price => 49.50)
