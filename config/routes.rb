e5a9f44a07c665440b0d9db58ac8c95a5f4ba833
xinxiang <xiang.wang@bozone.mobi>
2012-03-18 23:21:56 +0800

    add store

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Depot::Application.routes.draw do
+ get "store/index"
+
   resources :products
 
   # The priority is based upon order of creation:
@@ -57,4 +59,6 @@ Depot::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  root :to=>'store#index', :as=>'store'
+
 end
