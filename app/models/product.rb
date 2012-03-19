2b99908b834fd334fff9d50e9093d35492c6d9a5
waxiangzi <waxiangzi@gmail.com>
2012-03-19 09:54:45 +0800

    add cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 #coding: utf-8
 class Product < ActiveRecord::Base
 	default_scope :order=>'title'
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
 	validates :title, :description, :image_url, :presence=>true
 	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 	validates :title, :uniqueness => true #唯一
@@ -8,6 +10,16 @@ class Product < ActiveRecord::Base
 		:with => %r{\.(gif|jpg|png)$}i,
 		:message => '图片必需是GIF, JPG 或者 PNG 格式的URL!'
 	}
+
+	private 
+	def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			true
+		else
+			errors.add(:base, 'Line Items present')
+			false
+		end
+	end
 end
 
   
